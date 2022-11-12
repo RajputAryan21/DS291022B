d65a5f0e575b710c72702e48ef1ec58b49627ad3
RajputAryan21 <117184286+RajputAryan21@users.noreply.github.com>
2022-11-12 19:26:38 +0530

    Delete _3_conditional_statement directory

--- _3_conditional_statement/_1_conditional_statement.py
+++
@@ -1,7 +0,0 @@
-# Conditional Statement - it helps us to decide the further code on the bases of the output of conditions
-
-# if statement
-# if..else statement
-# elif statement
-# ladder if
-# nested if
